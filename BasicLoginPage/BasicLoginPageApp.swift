03ff71bfb439326318c2e737d18982ebd0a57ced
RehanKahtri <rehankhatri502@gmail.com>
2025-12-03 15:34:30 +0530

    Initial Commit

---
+++ BasicLoginPage/BasicLoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicLoginPageApp.swift
+//  BasicLoginPage
+//
+//  Created by student on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BasicLoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
